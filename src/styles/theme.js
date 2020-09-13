db682def012cc13c50b5539708c3fdaecae52732
William Chan <wchan1992@gmail.com>
2020-09-13 04:24:41 -0700

    Refactor styles

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+    },
+    secondary: {
+      main: "#000000",
+    },
+  },
+});
+
+export default theme;
