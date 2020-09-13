8a3362e9da438c36652c233f7862f49e5fe07990
William Chan <wchan1992@gmail.com>
2020-09-13 04:24:02 -0700

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import theme from './theme';
+import './styles/index.css';
+import App from './components/App';
+import theme from './styles/theme';
 import { ThemeProvider } from '@material-ui/core/styles';
 
 import * as serviceWorker from './serviceWorker';
